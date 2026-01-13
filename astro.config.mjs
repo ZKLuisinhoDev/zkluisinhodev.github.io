8083589af5cbcc9658149e8e637ab588a1902a8c
ZKLUIS999 <lassoluis03@gmail.com>
2026-01-13 17:35:13 -0500

    new try

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  // Reemplaza con tu dominio o 'https://<tu-usuario>.github.io'
-  site: "https://ZKLuisinhoDev.github.io",
-  // Si despliegas en <tu-usuario>.github.io/<nombre-repo>, agrega base: '/nombre-repo'
-  base: "/luisinhodev.github.io",
+  site: "https://zkluisinhodev.github.io",
+  // ⚠️ NO base cuando el repo es username.github.io
   vite: {
     plugins: [tailwindcss()],
     server: {
