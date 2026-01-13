eeef572665ef845ef76a5d9fbaf85e78da0c5f2b
ZKLUIS999 <lassoluis03@gmail.com>
2026-01-13 18:04:37 -0500

    Fix base path to root for User Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://zkluisinhodev.github.io",
-  base: "zkluisinhodev", 
+  base: "/", 
 
   vite: {
     plugins: [tailwindcss()],
